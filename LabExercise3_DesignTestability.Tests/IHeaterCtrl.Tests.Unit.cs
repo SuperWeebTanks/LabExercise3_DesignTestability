cfa36a48db3978d35698846475368d41bb29cb9f
SuperWeebTanks <trilleo12@live.dk>
2019-02-13 11:20:34 +0100

    Added tests for FakeHeater

--- LabExercise3_DesignTestability.Tests/IHeaterCtrl.Tests.Unit.cs
+++ LabExercise3_DesignTestability.Tests/IHeaterCtrl.Tests.Unit.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ECS.Legacy;
+using NUnit.Framework;
 
 namespace LabExercise3_DesignTestability.Tests
 {
@@ -23,5 +24,41 @@ namespace LabExercise3_DesignTestability.Tests
         {
             return true; 
         }
+    }
+
+    [TestFixture]
+    class FakeHeaterTests
+    {
+        private string _FakeTestStringTurnOn { get; set; }
+        private string _FakeTestStringTurnOff { get; set; }
+        private readonly FakeHeater _uut = new FakeHeater(); 
+
+        [SetUp]
+        public void Setup()
+        {
+            _FakeTestStringTurnOn = "Fake Turn On";
+            _FakeTestStringTurnOff = "Fake Turn Off"; 
+        }
+
+        [Test]
+        public void RunSelfTest_RunSelfTestSuccess_ReturnTrue()
+        {
+            Assert.That(_uut.RunSelfTest, Is.EqualTo(true)); 
+        }
+
+        [Test]
+        public void TurnOn_CheckIfTurnOnIsCalled_StringEqualTrue()
+        {
+            _uut.TurnOn();
+            //Assert.That(_FakeTestStringTurnOn, Is.Equal(_uut.TurnOn_));
+        }
+
+        [Test]
+        public void TurnOff_CheckIfTurnOffIsCalled_StringEqualTrue()
+        {
+            _uut.TurnOff();
+            //Assert.That(_FakeTestStringTurnOff, Is.Equal(_uut.TurnOn_)); 
+        }
+
     }
 }
