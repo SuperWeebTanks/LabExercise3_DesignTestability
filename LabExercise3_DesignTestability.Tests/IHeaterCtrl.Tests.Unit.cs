9b8c5e5d4a2cd3cb6d98aa832868f266bc31cba6
SuperWeebTanks <trilleo12@live.dk>
2019-02-13 10:49:22 +0100

    Added Fakes

--- LabExercise3_DesignTestability.Tests/IHeaterCtrl.Tests.Unit.cs
+++ LabExercise3_DesignTestability.Tests/IHeaterCtrl.Tests.Unit.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECS.Legacy;
 
 namespace LabExercise3_DesignTestability.Tests
 {
-    class IHeaterCtrl
+    class FakeHeater : IHeaterCtrl
     {
+        public void TurnOn()
+        {
+
+        }
+
+        public void TurnOff()
+        {
+
+        }
+
+        public bool RunSelfTest()
+        {
+            return true; 
+        }
     }
 }
--- LabExercise3_DesignTestability.Tests/ISensorCtrl.Tests.Unit.cs
+++ LabExercise3_DesignTestability.Tests/ISensorCtrl.Tests.Unit.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECS.Legacy;
 
 namespace LabExercise3_DesignTestability.Tests
 {
-    class ISensorCtrl
+    class FakeSensor : ISensorCtrl
     {
+        public int GetTemp()
+        {
+            return 0;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true; 
+        }
     }
 }
