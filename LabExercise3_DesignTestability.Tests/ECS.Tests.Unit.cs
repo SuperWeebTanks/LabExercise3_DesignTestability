8f71bc1ebf5d1887948f9520ba1b0b868d211082
MH <mathhan01@gmail.com>
2019-02-19 13:50:37 +0100

    Added 3 NSubstitute test cases

--- LabExercise3_DesignTestability.Tests/ECS.Tests.Unit.cs
+++ LabExercise3_DesignTestability.Tests/ECS.Tests.Unit.cs
@@ -9,26 +9,49 @@ using NUnit.Framework;
 
 namespace LabExercise3_DesignTestability.Tests
 {
-      [TestFixture]
-      class NSubstituteTests
-      {
-          private IHeaterCtrl _heater;
-          private ISensorCtrl _sensor;
-          private ECS.Legacy.ECS _uut;
+    [TestFixture]
+    class NSubstituteTests
+    {
+        private IHeaterCtrl _heater;
+        private ISensorCtrl _sensor;
+        private ECS.Legacy.ECS _uut;
 
-          [SetUp]
-          public void Setup()
-          {
-              IHeaterCtrl _heater = Substitute.For<IHeaterCtrl>();
-              ISensorCtrl _sensor = Substitute.For<ISensorCtrl>();
+        [SetUp]
+        public void Setup()
+        {
+            _heater = Substitute.For<IHeaterCtrl>();
+            _sensor = Substitute.For<ISensorCtrl>();
 
-              var _uut = new ECS.Legacy.ECS(20, _heater, _sensor);
-          }
+            _uut = new ECS.Legacy.ECS(20, _heater, _sensor);
+        }
 
-          [Test]
-          public void 
+        [Test]
+        public void Regulate_TempUnderThreshold_HeaterDoesNotTurnOff()
+        {
+            _sensor.GetTemp().Returns(_uut.GetThreshold() - 10);
+            _uut.Regulate();
+            _heater.DidNotReceive().TurnOff();
+        }
 
+        [Test]
+        public void Regulate_TempOverThreshold_HeaterDoesNotTurnOn()
+        {
+            _sensor.GetTemp().Returns(_uut.GetThreshold() + 10);
+            _uut.Regulate();
+            _heater.DidNotReceive().TurnOn();
+        }
 
-      }
+        [Test]
+        public void Regulate_TempUnderThreshold_HeaterTurnOn3times()
+        {
+            _sensor.GetTemp().Returns(_uut.GetThreshold() - 10);
+            _uut.Regulate();
+            _sensor.GetTemp().Returns(_uut.GetThreshold() - 5);
+            _uut.Regulate();
+            _sensor.GetTemp().Returns(_uut.GetThreshold() - 2);
+            _uut.Regulate();
+            _heater.Received(3).TurnOn();
+        }
+
     }
 }
